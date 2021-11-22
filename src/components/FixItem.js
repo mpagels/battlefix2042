748a112fccf9a4284fc3b563db4d03b2426fb58c
Martin <office@martinpagels.de>
2021-11-22 01:22:14 +0100

    Minor styling change in FixItem

--- src/components/FixItem.js
+++ src/components/FixItem.js
@@ -2,18 +2,23 @@ import styled from "styled-components/macro";
 import { MdOutlineFavoriteBorder, MdOutlineFavorite } from "react-icons/md";
 import { HiExternalLink } from "react-icons/hi";
 
-const heartStyle = { color: "#26ffdf", cursor: "pointer" };
+const heartStyle = {
+  color: "#26ffdf",
+  cursor: "pointer",
+};
 
-export default function FixItem({ isFavorite, isFixed }) {
+export default function FixItem({ isFavorite, isFixed, feature }) {
   return (
     <Wrapper>
       <Info>
-        {isFavorite ? (
-          <MdOutlineFavorite size="30" style={heartStyle} />
-        ) : (
-          <MdOutlineFavoriteBorder size="30" style={heartStyle} />
-        )}
-        <FixTitel>No single-player story/campaign mode</FixTitel>
+        <FavoriteWrapper>
+          {isFavorite ? (
+            <MdOutlineFavorite size="30" style={heartStyle} />
+          ) : (
+            <MdOutlineFavoriteBorder size="30" style={heartStyle} />
+          )}
+        </FavoriteWrapper>
+        <FixTitel>{feature}</FixTitel>
       </Info>
       {isFixed ? (
         <Additional>
@@ -33,13 +38,18 @@ export default function FixItem({ isFavorite, isFixed }) {
 const Additional = styled.div`
   display: flex;
   align-items: center;
-  margin: 3px 40px;
+  margin: 1px 40px;
+`;
+
+const FavoriteWrapper = styled.div`
+  width: 30px;
+  height: 30px;
 `;
 
 const Fixed = styled.div`
   background-color: #26ffdf;
   padding: 0.4em;
-  font-size: 0.7em;
+  font-size: 0.6em;
 `;
 
 const FixTitel = styled.p`
@@ -57,8 +67,11 @@ const PatchNotes = styled.a`
   display: flex;
   align-items: center;
   gap: 3px;
-  font-size: 0.8em;
+  font-size: 0.7em;
   margin: 0 10px;
   color: white;
 `;
-const Wrapper = styled.li``;
+const Wrapper = styled.li`
+  font-family: Roboto;
+  margin: 10px 0;
+`;
