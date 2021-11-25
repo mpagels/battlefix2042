f33a93b4bab237057558ef135107f318d589086f
Martin <office@martinpagels.de>
2021-11-25 20:39:19 +0100

    WIP: add first about draft

---
+++ src/pages/about.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h2>I'm Martin. 👋</h2>
+      <h3>Gamer, filmmaker & web developer.</h3>
+
+      <p>
+        💓 I love playing Battlefield! <br /> I want that the game to succeed,
+        because like a lot of other people despite the current state of the
+        game, I think it has a lot of potential.
+      </p>
+
+      <p>
+        I made the app for everybody who wants to keep track of the current
+        state of the game and give the possibility to keep track of things that
+        are important for each gamer personally. Let's face it: Not everyone is
+        sad about the fact that they are no single player campaign. ;-){" "}
+      </p>
+
+      <p>
+        With this app you can select the features you really want in the game
+        and I will update the list on every patch note. So you can relax and
+        check the app once in a while.
+      </p>
+
+      <p>Have fun, see you on the battlefield.</p>
+
+      <p>Martin</p>
+
+      <p>
+        If you want to get in touch with me or have a feature idea for the app,
+        write me an email or a pm on Reddit.
+      </p>
+
+      <p>
+        <a href="mailto:battlefix2042@gamil.com">battlefix2042@gamil.com</a>
+      </p>
+    </div>
+  );
+}
